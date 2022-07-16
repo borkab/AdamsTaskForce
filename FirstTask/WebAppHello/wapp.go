703cf194a88bea8da07e35239462614fd35b2d2d
borkab <borbala.botond@gmail.com>
2022-07-17 00:31:38 +0200

    outputs commented

--- FirstTask/WebAppHello/wapp.go
+++ FirstTask/WebAppHello/wapp.go
@@ -13,3 +13,13 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, world!")
 }
+
+/*
+curl -X GET http://localhost:8080/
+curl: (7) Failed to connect to localhost port 8080 after 0 ms: Connection refused
+*/
+//BUT:
+/* go run wapp.go
+And navigate to http://localhost:8080 in the browser
+it gives: Hello, world!
+*/
